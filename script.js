75077e1c69268f322902961bd180584a5357ae33
khalidimo <khalidimo@gmail.com>
2018-12-26 21:36:17 +0100

    creation of the function: createElementLien (link)

--- script.js
+++ script.js
@@ -19,3 +19,41 @@ var listeLiens = [
         auteur: "annie.zette"
     }
 ];
+
+// Crée et renvoie un élément DOM affichant les données d'un lien
+// Le paramètre lien est un objet JS représentant un lien
+function creerElementLien(lien) {
+    var titreLien = document.createElement("a");
+    titreLien.href = lien.url;
+    titreLien.style.color = "#428bca";
+    titreLien.style.textDecoration = "none";
+    titreLien.style.marginRight = "5px";
+    titreLien.appendChild(document.createTextNode(lien.titre));
+
+    var urlLien = document.createElement("span");
+    urlLien.appendChild(document.createTextNode(lien.url));
+
+    // Cette ligne contient le titre et l'URL du lien
+    var ligneTitre = document.createElement("h4");
+    ligneTitre.style.margin = "0px";
+    ligneTitre.appendChild(titreLien);
+    ligneTitre.appendChild(urlLien);
+
+    // Cette ligne contient l'auteur
+    var ligneDetails = document.createElement("span");
+    ligneDetails.appendChild(document.createTextNode("Ajouté par " + lien.auteur));
+
+    var divLien = document.createElement("div");
+    divLien.classList.add("lien");
+    divLien.appendChild(ligneTitre);
+    divLien.appendChild(ligneDetails);
+
+    return divLien;
+}
+
+var contenu = document.getElementById("contenu");
+// Parcours de la liste des liens et ajout d'un élément au DOM pour chaque lien
+listeLiens.forEach(function (lien) {
+    var elementLien = creerElementLien(lien);
+    contenu.appendChild(elementLien);
+});
